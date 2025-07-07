373c66e3fe4b8fc5ff3647caca6cccb20a9b9285
cyril.wolfangel@talan.com <cyril.wolfangel@talan.com>
2025-07-07 10:33:03 +0200

    first commit new app

--- evaluate/LLM_accelerated/sglang_qwen.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# specify path of your model here
-python3 -m sglang.launch_server --model-path /root/autodl-tmp/mentalqlm_all --host 0.0.0.0
--- functions/__init__.py
+++
@@ -1,3 +0,0 @@
-from .data_utils import *
-from .train_utils import *
-from .model import *
